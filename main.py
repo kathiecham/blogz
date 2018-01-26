6f83ee84828306f43f09681d4adb04593c1715d0
Kate <kathyswg@hotmail.com>
2018-01-26 16:04:14 -0500

    Final code for Build a Blog

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class Blog(db.Model):
 @app.route('/blog', methods=['GET'])
 def index():
 
-    if request.args:
+    if request.args.get("id"):
         blog_id = request.args.get("id")
         blog = Blog.query.get(blog_id)
 
@@ -29,38 +29,34 @@ def index():
 
     else:
         blogs = Blog.query.all()
-
-        return render_template('blog.html', title="Build A Blog", 
-            blogs=blogs)
+        return render_template('blog.html', blogs=blogs)
 
 @app.route('/add_post', methods=['GET', 'POST'])
 def add_blog():
     if request.method == 'GET':
-        return render_template('/add_post.html', title="Add Blog Entry")
+        return render_template('add_post.html')
 
     if request.method == 'POST':
-        blog_title = request.form['title']
-        blog_body = request.form['body']
+        title = request.form['title']
+        body = request.form['body']
         title_error = ""
         body_error = ""
 
-        if len(blog_title) < 1:
+        if not title:
             title_error = "Please fill in the title"
 
-        if len(blog_body) < 1:
+        if len(body) < 1:
             body_error = "Please fill in the body"
 
         if not title_error and not body_error:
-            new_blog = Blog(blog_title, blog_body)
+            new_blog = Blog(title, body)
             db.session.add(new_blog)
             db.session.commit()
             query_param_url = "/blog?id=" + str(new_blog.id)
             return redirect(query_param_url)
-            # or: return render_template('singleBlogEntry.html', blog=blog)
 
         else:
-            return render_template('/add_post.html', title="Add Blog Entry", 
-                title_error=title_error, body_error=body_error)
+            return render_template('add_post.html', title_error=title_error, body_error=body_error)
 
 if __name__ == '__main__':
     app.run()
